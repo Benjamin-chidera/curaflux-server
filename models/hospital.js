628d6f66baf09ed4de7080eb3df4b2a70f9d6f78
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-10-05 00:15:43 +0100

    feat: added new end points to api

--- models/hospital.js
+++ models/hospital.js
@@ -1,42 +1,79 @@
 import mongoose, { Schema } from "mongoose";
 
 // Define the schema
-const HospitalSchema = new Schema({
-  adsNote: {
-    type: String,
-    default: "",
-  },
-  name: {
-    type: String,
-    required: true, // Ensure the name is required
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: String, // You can adjust the type if it's in hours, days, etc.
-    required: true,
-  },
-  payRate: {
-    type: Number, // Assuming payRate is a number, you can change it to String if necessary
-    required: true,
-  },
-  specialization: {
-    type: String,
-    required: true,
-  },
-  licenseRequired: {
-    type: String, // Assuming licenseRequired is a boolean (true/false)
-    required: true,
-  },
-});
+const ShiftSchema = new Schema(
+  {
+    adsNote: {
+      type: String,
+      default: "",
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    payRate: {
+      type: String,
+      required: true,
+    },
+    jobType: {
+      type: String,
+      required: true,
+    },
+    specialRequirement: {
+      type: String,
+      required: true,
+    },
 
-// Create the model
-export const Hospital = mongoose.model("Hospital", HospitalSchema);
+    skills: {
+      type: [String],
+    },
 
-// Export the model
+    hospital: {
+      type: mongoose.Types.ObjectId,
+      ref: "Healthcare",
+      required: true,
+    },
+
+    applicants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Application",
+      },
+    ],
+
+    shiftSupervisorName: {
+      type: String,
+      required: true,
+    },
+
+    shiftSupervisorPosition: {
+      type: String,
+      required: true,
+    },
+
+    shiftSupervisorEmail: {
+      type: String,
+      required: true,
+    },
+
+    shiftSupervisorPhoneNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Shifts = mongoose.model("Shifts", ShiftSchema);
+
+
--- routes/hospitalRoute.js
+++ routes/hospitalRoute.js
@@ -3,6 +3,8 @@ import {
   createHospital,
   getAllHospitals,
   getHospitalById,
+  getShiftForAHealthCare,
+  searchShift,
 } from "../controllers/hosiptalController.js";
 import {
   forgottenPassword,
@@ -10,16 +12,38 @@ import {
   signIn,
   signUp,
 } from "../controllers/hospitalAuthController.js";
+import { permission, protect } from "../middleware/auth.js";
 
 const router = Router();
 
-router.post("/createHospital", createHospital);
-router.get("/getAllHospitals", getAllHospitals);
-router.get("/getHospitalById/:HospitalId", getHospitalById);
+router.post(
+  "/createHospital",
+  protect,
+  permission("healthcare"),
+  createHospital
+);
+router.get(
+  "/getAllHospitals",
+  protect,
+  permission("healthcare", "locum"),
+  getAllHospitals
+);
+router.get("/q/shifts", searchShift);
 
+router.get(
+  "/getHospitalById/:HospitalId",
+  protect,
+  permission("healthcare", "locum"),
+  getHospitalById
+);
+
+router.get("/getShiftForAHealthCare/:hospitalId", getShiftForAHealthCare);
+
+
+// Authentication
 router.post("/register", signUp);
 router.post("/login", signIn);
 router.post("/forgot-Password", forgottenPassword);
-router.patch("/reset-Password/:token", resetPassword);
+router.patch("/reset-Password/:healthcareToken", resetPassword);
 
 export default router;
